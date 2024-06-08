3a70526a587fa6df944aea38141fe9250bf65654
imshaw695 <ivan.shaw695@gmail.com>
2024-06-08 22:27:57 +0100

    end

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 4;  // mut is short for mutable, you must delcare a variable as mutable if you want to change it later on.
+    println!("x is {}", x);
+    x = 5;
+    println!("x is {}", x);
 }
