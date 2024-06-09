9e61d63cac98ec1fff110efd5cb4955a91520356
Ivan Shaw <ivan.shaw695@gmail.com>
2024-06-09 11:50:02 +0100

    web server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-fn main() {
-    let mut x = 4;  // mut is short for mutable, you must delcare a variable as mutable if you want to change it later on.
-    println!("x is {}", x);
-    x = 5;
-    println!("x is {}", x);
+use actix_files::Files;
+use actix_web::{App, HttpServer, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/", "./dist").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
