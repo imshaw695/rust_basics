ba0dd486160e80d6084b79755fa0e3040a7c2471
Ivan Shaw <ivan.shaw695@gmail.com>
2024-06-09 11:58:40 +0100

    rust serving react SPA

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use actix_files::Files;
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(Files::new("/", "./dist").index_file("index.html"))
+            .service(Files::new("/", "./front_end/dist").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .run()
